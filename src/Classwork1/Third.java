d9c754587ef88db042b736d070bc63fbce35643e
mari sikharulidze <mary.sikharulidze@gmail.com>
2022-03-25 16:20:33 +0400

    k

--- src/Classwork1/Third.java
+++ src/Classwork1/Third.java
@@ -1,14 +1,24 @@
 package Classwork1;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Third {
     public static void main(String[] args) {
 
-        int a = 0;
-        int b = 20;
+        int a, b, i, even=0, odd=0, rand;
 
-        for(a=0; a<b; a++){
+        a = 1;
+        b = 20;
 
+        for(i=0; i<10; i++){
+            rand = ThreadLocalRandom.current().nextInt(a,b+1);
+            if(rand%2 == 0) even++;
+            else odd++;
+            System.out.print(rand + " ");
         }
+        System.out.println();
+        System.out.println("even : " + even);
+        System.out.println("odd : " + odd);
     }
 
 
