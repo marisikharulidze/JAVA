d4c5a06545319f40660a0b90700c3fb78f3af85c
mari sikharulidze <mary.sikharulidze@gmail.com>
2022-03-25 16:33:50 +0400

    k

--- src/Classwork1/FirstChild.java
+++ src/Classwork1/FirstChild.java
@@ -4,7 +4,7 @@ import Homework_2.Constructor.First;
 
 import java.util.Scanner;
 
-public class FirstChild extends First {
+public class FirstChild extends Second {
     public static void main(String[] args) {
         method_1();
         System.out.println(method_2());
@@ -13,9 +13,8 @@ public class FirstChild extends First {
     public static int y;
 
     public static void method_1(){
-        System.out.println("Insert : ");
         Scanner scan = new Scanner(System.in);
-        x = scan.nextInt();
+        Second construct = new Second();
         y = scan.nextInt();
     }
     public static int method_2(){return x + y;}
